aadf4c2fbdf0b6781449a7e62bfe456868d08bb8
David Hui <dythui1@gmail.com>
2017-06-08 19:55:43 +0100

    added node object

--- noughtsCrosses.py
+++ noughtsCrosses.py
@@ -1,2 +1,12 @@
-#!/usr/bin/env/python
-"""A game playing """
+#!/usr/bin/env python
+"""Noughts and crosses in Python"""
+import numpy as np
+
+board = [['', '', ''], ['', '', ''], ['', '', '']]
+
+class node:
+    'node within a tree where moves can be made'
+
+    def __init__(self, board):
+        'initialise new node with a board'
+        self.board = board
