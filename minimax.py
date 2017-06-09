debeff9faf4b0d7a0862f20fbcc41d61c7171f44
David Hui <dythui1@gmail.com>
2017-06-09 09:13:45 +0100

    removed superfluous debug test cases

--- minimax.py
+++ minimax.py
@@ -2,7 +2,6 @@
 """the minimax procedure with alpha beta pruning"""
 from noughtsCrosses import *
 
-x = 0
 
 class node:
     'a node in a list tree'
@@ -13,17 +12,23 @@ class node:
         self.board = board
         self.player = player
         self.winner = evaluate(board)
-
-        global x
-        x += 1
-        
         # if a winner does not exist, swap the players and do all possible moves
         if self.winner == None:
             self.next = nextPlayer(self.player)
             self.moves = [node(self.next, move) if (move != None) else None
                           for move in moveAll(self.next, self.board)]
 
+            
+def largestIndex(l):
+    'return the index of the largest item in l'
+    return l.index(max(l))
+
+
+def smallestIndex(l):
+    'return the index of the smallest item in l'
+    return l.index(min(l))
+
 
 
 me = node(1, initialBoard)
-print x
+print x, o, draws
